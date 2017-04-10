94bc71e6fec6e01bd0fd3763062a1309b1ca7e8e
daniel alberto bernal <tamarindo@MacBook-Air-de-daniel.local>
2017-04-10 11:31:45 -0500

    seccion one

--- source/sections/prueba/libs/scripts/component.js
+++ source/sections/prueba/libs/scripts/component.js
@@ -1,23 +1,57 @@
 // TODO: code logic here
 (function () {
-    new Vue({
-            el: "#app",
-            data: {
-                name: "",
-                gender: 'm',
-                users: [
-                    { name: 'Dani' },
-                    { name: 'Carlos' },
-                    { name: 'Juanfe' },
-                ],
-                message: 'Holi soy un mensaje personalizable',
-                seen: true, //seen:false,
-                color: 'blue',
-            },
-            methods: {
-                changeColor: function (color) {
-                   this.color = color;
+
+    Vue.component( 'menu-accesibility', {
+        template:"#menu",
+        data: function(){
+            return { 
+                menu: {
+                    items: [
+                        {
+                            text: "item 1",
+                            ariaExpandend: false,
+                            submenu: {
+                                items :[
+                                    { text: "item 1"},
+                                    { text: "item 2"},
+                                    { text: "item 3"},
+                                ],
+                                ariaHidden: false
+                            }
+                        },
+                        {   
+                            text: "item 2",
+                            ariaExpandend: false,
+                            submenu: {
+                                items :[
+                                    { text: "item 1", },
+                                    { text: "item 2"},
+                                    { text: "item 3"},
+                                ],
+                                ariaHidden: false
+                            }
+                        }
+                    ]
                 }
             }
-        });
+        },
+        methods: {
+            enterItem: function(item) {
+            item.ariaExpandend = ! item.ariaExpandend;
+            item.submenu.ariaHidden = ! item.submenu.ariaHidden;
+                
+            },
+
+            subItemEvent: function() {
+                console.log(event);   
+            },
+        }
+    });
+
+    new Vue({
+        el: "#app1",
+        data: {},
+    });
+
 })();
+
