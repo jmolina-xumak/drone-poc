4989f1707ff54b3014495ffd21446fce28c2d068
Front-End <front-end@Front-End-MacBook-Pro.local>
2017-05-10 11:08:40 -0500

    best practices for templates management  with ajax

--- Prototypes/xumakSite2017/source/sections/topbanner/libs/scripts/component.js
+++ Prototypes/xumakSite2017/source/sections/topbanner/libs/scripts/component.js
@@ -1,8 +1,10 @@
 
 // TODO: code logic here
 
-  Vue.component('hero', {
-            template: '#hero',
+Vue.component('hero', function heroCreator(resolve, reject) {
+    $.ajax('/assets/templates/hero-template.html').done((template)=>{
+        resolve({
+            template: template,
             props: ['bg-image', 'bg-color', 'title', 'link-url'],
             computed: {
                 background: function () {
@@ -10,39 +12,41 @@
                 }
             },
             mounted: function () {
-                
+
                 var tl = new TimelineLite(),
                     title = $(".Hero-title"),
-                    cta= $(".Hero-cta"),
-                    background= $(".Hero");
-                    
-                    tl.from(background, 1.0, { opacity: 0}, "0" );
-                    tl.from(title, 1, {  opacity: 0, top :100},"-=0.8");
-                    tl.from(cta, 1, { opacity: 0, top :110},"-=0.7");
-                    tl.play();
-                 }
-                });
-            
-        Vue.component('xumak-text', {
-            template: '<div><slot></slot></div>',
-            mounted: function() {
-                /*
-                var tlXumakText = new TimelineMax();
-                var tlXumakText2 = new TimelineMax();
-                XumakText = $("#XumakText");
-                tlXumakText.to(XumakText, 1, { left :'-10px'});
-                tlXumakText.play();
+                    cta = $(".Hero-cta"),
+                    background = $(".Hero");
 
-                new ScrollMagic.Scene({
-                    duration: '30%'
-                })
-                .setTween(tlXumakText2)
-                .triggerHook(0)
-                .triggerElement('#ColumnsAnimation')
-                .addIndicators({name: " ColumnsAnimation "})
-                .addTo(this.controller);*/
+                tl.from(background, 1.0, { opacity: 0 }, "0");
+                tl.from(title, 1, { opacity: 0, top: 100 }, "-=0.8");
+                tl.from(cta, 1, { opacity: 0, top: 110 }, "-=0.7");
+                tl.play();
             }
-        }); 
-              
+        });
+    });
+    
+});
 
-     
+Vue.component('xumak-text', {
+    template: '<div><slot></slot></div>',
+    mounted: function () {
+        /*
+        var tlXumakText = new TimelineMax();
+        var tlXumakText2 = new TimelineMax();
+        XumakText = $("#XumakText");
+        tlXumakText.to(XumakText, 1, { left :'-10px'});
+        tlXumakText.play();
+
+        new ScrollMagic.Scene({
+            duration: '30%'
+        })
+        .setTween(tlXumakText2)
+        .triggerHook(0)
+        .triggerElement('#ColumnsAnimation')
+        .addIndicators({name: " ColumnsAnimation "})
+        .addTo(this.controller);*/
+    }
+});
+
+
