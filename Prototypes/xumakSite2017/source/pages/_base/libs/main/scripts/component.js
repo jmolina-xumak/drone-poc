da4efa882aa3853c095206f3ab7e59e1c5c0964f
alejandro navarro <anavarro@xumak.com>
2017-05-22 15:34:44 -0500

    Created vue component and content structure of the main navigation

--- Prototypes/xumakSite2017/source/pages/_base/libs/main/scripts/component.js
+++ Prototypes/xumakSite2017/source/pages/_base/libs/main/scripts/component.js
@@ -1,73 +1,73 @@
 // TODO: code logic here
-(function(){
-    Vue.component( 'xmk-input', {
-        props: [
-            'name',
-            'label',
-            'status',
-            'validatorMessage',
-            'placeholder'
-        ],
-        data: function () {
-            return {
-                focused : false,
-                input: ''
-            }
-        },
-        methods:{
-            focusIn: function(){
-                this.focused = true;
-            },
-            focusOut:function(){
-                this.focused = false;
-            }
-        },
-        computed:{
-          hasStatus : function (){
-              return (this.input.length  > 0 || this.focused) ? 'MainInput--focused ' + this.status : '' ;
-          }
-        },
-        template:'#xmk-input'
-    } );
 
-    Vue.component('xmk-select', {
-    	props: ['value', 'id', 'data', 'label', 'placeholder'],
-      data: function () {
+Vue.component( 'xmk-input', {
+    props: [
+        'name',
+        'label',
+        'status',
+        'validatorMessage',
+        'placeholder'
+    ],
+    data: function () {
         return {
-          selected: null,
-          isOpen: false,
-          isHidding: false,
-          options: []
+            focused : false,
+            input: ''
         }
-      },
-      created: function(){
-      	this.options = this.data;
-      },
-      methods: {
-      	hidden: function(){
-        	this.isOpen = false;
+    },
+    methods:{
+        focusIn: function(){
+            this.focused = true;
         },
-      	toggle: function() {
-          if ( !this.disabled ) {
-            this.isOpen = !this.isOpen;
-          }
-        },
-        update: function( item ) {
-    	    this.selected = item;
-        	this.options = this.options.map(function(it){
-          	it.selected = false;
-            return it;
-          });
-          this.$emit('input', this.selected.value);
+        focusOut:function(){
+            this.focused = false;
         }
-      },
-      computed: {
+    },
+    computed:{
         hasStatus : function (){
-            return (this.selected || this.isOpen) ;
+            return (this.input.length  > 0 || this.focused) ? 'MainInput--focused ' + this.status : '' ;
         }
-      },
-      template: '#xmk-select'
-    });
+    },
+    template:'#xmk-input'
+} );
 
-    new Vue({}).$mount('#xmkApp');
-})();
+Vue.component('xmk-select', {
+    props: ['value', 'id', 'data', 'label', 'placeholder'],
+    data: function () {
+    return {
+        selected: null,
+        isOpen: false,
+        isHidding: false,
+        options: []
+    }
+    },
+    created: function(){
+    this.options = this.data;
+    },
+    methods: {
+    hidden: function(){
+        this.isOpen = false;
+    },
+    toggle: function() {
+        if ( !this.disabled ) {
+        this.isOpen = !this.isOpen;
+        }
+    },
+    update: function( item ) {
+        this.selected = item;
+        this.options = this.options.map(function(it){
+        it.selected = false;
+        return it;
+        });
+        this.$emit('input', this.selected.value);
+    }
+    },
+    computed: {
+    hasStatus : function (){
+        return (this.selected || this.isOpen) ;
+    }
+    },
+    template: '#xmk-select'
+});
+
+new Vue({}).$mount('#xmkApp');
+
--- Prototypes/xumakSite2017/source/sections/mainNavigation/libs/scripts/component.js
+++ Prototypes/xumakSite2017/source/sections/mainNavigation/libs/scripts/component.js
@@ -1,2 +1,29 @@
 
 // TODO: code logic here
+Vue.component('mainNavigation', function mainNavCreator(resolve, reject) {
+    $.ajax('/assets/templates/main-navigation.html').done(function(template){
+        resolve({
+            props:['structure', 'dataImagePath', 'dataImageAltText', 'dataRootPath'],
+            template: template,
+            data: function(){
+                return {
+                    menu: {}
+                }
+            },
+            created: function(){
+                this.menu = this.structure;
+            },
+            methods: {
+                toggleSubMenu: function(hasChilds){
+                    if(hasChilds){
+                        //toggleMenu
+                        alert('Toggle menu');
+                    } else {
+                        return false;
+                    }
+
+                }
+            }
+        })
+    });
+});
