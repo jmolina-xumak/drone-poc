4d041925f54dde281e063b9266ff4eb95192721c
alejandro navarro <anavarro@xumak.com>
2017-05-23 17:05:15 -0500

    Added Vue events , added overlay, added styles for mobile

--- Prototypes/xumakSite2017/source/sections/mainNavigation/libs/scripts/component.js
+++ Prototypes/xumakSite2017/source/sections/mainNavigation/libs/scripts/component.js
@@ -11,18 +11,53 @@ Vue.component('mainNavigation', function mainNavCreator(resolve, reject) {
                 }
             },
             created: function(){
+                var vm = this;
                 this.menu = this.structure;
+                this.menu.pages = this.menu.pages.map(function(el) {
+                    vm.$set(el,'isExpanded', false);
+                    return el;
+                });
+                vm.$set(this.menu, 'isVisible', false);
             },
             methods: {
-                toggleSubMenu: function(hasChilds){
-                    if(hasChilds){
+                toggleSubMenu: function(item){
+                    let isExpanded = !item.isExpanded;
+
+                    this.menu.pages = this.menu.pages.map(function(el){
+                        el.isExpanded = false;
+                        return el;
+                    });
+                    if(isExpanded === true ){
+                        item.isExpanded = isExpanded;
+                    }
+
+/*                    if(hasChilds){
                         //toggleMenu
                         alert('Toggle menu');
                     } else {
                         return false;
-                    }
+                    }*/
 
+                },
+                toggleMenu: function() {
+                    this.menu.isVisible = !this.menu.isVisible;
+
+                    
+
+                    if ( this.menu.isVisible ) {
+                        $('<div class="MainNavigation-overlay"/>').appendTo('body').animate({
+                            opacity: .8
+                        }, 500);
+                      
+                    } else {
+                        $('.MainNavigation-overlay').animate({
+                            opacity: 0
+                        }, 500, function() {
+                            $(this).remove();
+                        });
+                    }
                 }
+
             }
         })
     });
