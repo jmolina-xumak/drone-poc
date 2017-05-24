a7d23b528823635e4b3c14af7c019e46f43442d6
Daniel Bernal <dbernal@xumak.com>
2017-05-24 13:40:48 -0500

    component.js modified

--- Prototypes/xumakSite2017/source/sections/pageMotionTitle/libs/scripts/component.js
+++ Prototypes/xumakSite2017/source/sections/pageMotionTitle/libs/scripts/component.js
@@ -4,21 +4,20 @@ Vue.component( 'page-motion-title', {
     props: [
     'controller',
     ],
-    template: '<slot></slot>',
+    template: '<div><slot></slot></div>',
     mounted: function() {
 
       var tlXumakText = new TimelineMax (),
       tl2 = new TimelineLite(),
-      mainText = this.$el,
-
+      mainText = $(this.$el).find( '.pagaMotionTitle-component' ),
       distancia = mainText.offsetWidth * -1;
-
+      console.log( distancia );
       if ( document.body.scrollTop === 0 ) {
         tlXumakText.from( mainText, 1.5, { left: '100vw' }, '0' );
         tlXumakText.play( );
       }
 
-      tl2.to( $( this.$el ), 1, { left: distancia + 'px' }, '0' );
+      tl2.to( mainText, 1, { left: distancia + 'px' }, '0' );
       tl2.play();
 
       new ScrollMagic.Scene({
